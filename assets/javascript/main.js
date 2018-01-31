f3216af309d4eb84ce71ffb4753d4b47aeca5c9b
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-01-31 17:48:20 -0500

    firebase working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,37 +1,47 @@
 // Initialize Firebase
- var config = {
+var config = {
    apiKey: "AIzaSyC-F_1mpAYh-YZfrQXhY8x-UK2i3f29RAg",
    authDomain: "train-scheduler-dfd33.firebaseapp.com",
    databaseURL: "https://train-scheduler-dfd33.firebaseio.com",
    projectId: "train-scheduler-dfd33",
-   storageBucket: "",
+   storageBucket: "train-scheduler-dfd33.appspot.com",
    messagingSenderId: "947704542192"
  };
  firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#btn-primary").on("click", function () {
+var trainName = "";
+var destInput = "";
+var trainTimeInput = "";
+var freqInput = "";
+
+$(".btn-primary").on("click", function (event) {
+  event.preventDefault();
 
 var trainName = $('#TrainInput').val().trim();
 var destInput = $('#DestinationInput').val().trim();
 var trainTimeInput = $('#TrainTimeInput').val().trim();
 var freqInput = $('#FrequencyInput').val().trim();
 
-console.log(trainName);
-console.log(destInput);
-console.log(trainTimeInput);
-console.log(freqInput);
+var trainInfo = {
+  trainName: trainName,
+  destInput: destInput,
+  trainTimeInput: trainTimeInput,
+  freqInput: freqInput
+};
 
 // push values to firebase
-database.ref().push({
-  trainName: Train_Name,
-  destInput: Destination,
-  trainTimeInput: Train_Time,
-  freqInput: Frequency,
-});
+database.ref().push(trainInfo);
+
+console.log(trainInfo.trainName);
+console.log(trainInfo.destInput);
+console.log(trainInfo.trainTimeInput);
+console.log(trainInfo.freqInput);
 
 });
+
+
 
 //on click push to function database
 database.ref().on("child_added", function (childSnapshot) {
